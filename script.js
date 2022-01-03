90919d33e9b96ff9459394fdaaa7ec60a6ab2a6a
Harris Sultan <harris_sultan@outlook.com>
2022-01-02 20:41:31 -0500

    Added second button

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-document.getElementById("1stbutton").addEventListener("click", myFunction);
+document.getElementById("1stbutton").addEventListener("click", myFunction1);
 
-function myFunction() {
+function myFunction1() {
     document.getElementById("change").style.backgroundImage = "url('sunset.png')";
+}
+document.getElementById("2stbutton").addEventListener("click", myFunction2);
+
+function myFunction2() {
+    document.getElementById("change").style.backgroundImage = "url('nature.png')";
 }
