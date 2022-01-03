c3e8ce19e98ceac9cf2d7e5a6abf2c2fbff22f5d
Harris Sultan <harris_sultan@outlook.com>
2022-01-02 20:05:16 -0500

    event listener added

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("1stbutton").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("change").style.backgroundImage = "url('sunset.png')";
+}
